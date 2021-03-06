8a94ebd8d2e26d168b376dfe4952a88081a3f003
buildbobby <sethi.akshay@outlook.com>
2021-03-06 10:45:03 -0500

    added q1 solution

---
+++ 1.py
@@ -0,0 +1,19 @@
+# soln to : https://leetcode.com/problems/permutations/
+
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        
+        if len(nums) == 1:
+            return [nums] 
+        else:
+            permute_final = []
+            permute_before = self.permute(nums[1:])
+            
+            for perm in permute_before:
+                print (perm)
+                for i in range(len(perm)):
+                    permute_final.append(perm[:i] + [nums[0]] + perm[i:])
+                permute_final.append(perm + [nums[0]])
+                    
+        
+        return permute_final

f75e9633910a29e2a7d72c107b1e4f6dde4a893f
buildbobby <sethi.akshay@outlook.com>
2021-03-06 12:09:40 -0500

    q2 added

--- 2.py
+++ 2.py
@@ -1,2 +1,8 @@
-# sample question
-# sample sample
+# soln to : https://leetcode.com/problems/reverse-string/
+
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        n  = len(s)-1
+        for i in range(len(s)//2):
+            s[i],s[n-i] = s[n-i], s[i]
+        return s
